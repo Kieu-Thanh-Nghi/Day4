6afd865652bb9b0184a5565d8fd2f4cc9a2a8fa6
Kiều Thanh Nghị <souleater2222001@gmail.com>
2025-05-31 21:17:50 +0700

    ver0.2

--- Assets/Scripts/BasicDataPypes.cs
+++ Assets/Scripts/BasicDataPypes.cs
@@ -11,10 +11,16 @@ public class BasicDataPypes : MonoBehaviour
     private void Start()
     {
         test1();
+        test2();
+        test3();
+        test4();
+        test5();
+        test6();
     }
 
     void test1()
     {
+        Debug.Log("Test1");
         andResult = (true && false);
         orResult = (true || false);
         notResult = !true;
@@ -22,12 +28,14 @@ public class BasicDataPypes : MonoBehaviour
         Debug.Log("and: " + andResult);
         Debug.Log("or: " + orResult);
         Debug.Log("not: " + notResult);
+        print("\n");
     }
 
     [SerializeField] private int score = 85;
     void test2()
     {
-        if(score >= 90)
+        Debug.Log("Test2");
+        if (score >= 90)
         {
             Debug.Log("Grade: A");
         }else if(score >= 80)
@@ -38,11 +46,13 @@ public class BasicDataPypes : MonoBehaviour
         {
             Debug.Log("Grade: C");
         }
+        print("\n");
     }
 
     [SerializeField] private int day = 3;
     void test3()
     {
+        Debug.Log("Test3");
         switch (day)
         {
             case 1:
@@ -58,34 +68,41 @@ public class BasicDataPypes : MonoBehaviour
                 Debug.Log("Other day");
                 break;
         }
+        print("\n");
     }
 
     void test4()
     {
-        for(int i = 0; i < 5; i++)
+        Debug.Log("Test4");
+        for (int i = 0; i < 5; i++)
         {
             Debug.Log("i: " + i);
         }
+        print("\n");
     }
 
     void test5()
     {
+        Debug.Log("Test5");
         int i = 0;
         while(i < 5)
         {
             Debug.Log("i: " + i);
             i++;
         }
+        print("\n");
     }
 
     [SerializeField] private int i = 0;
     void test6()
     {
+        Debug.Log("Test6");
         do
         {
             Debug.Log("i: " + i);
             i++;
         } while (i < 5);
+        print("\n");
     }
 }
 
